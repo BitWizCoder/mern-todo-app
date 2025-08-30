7d6f504ff32a6fc46793dea338636afd62d4b849
Muhammad Noman <mhmdnoman01@gmail.com>
2025-08-30 17:49:53 +0600

    setup fornt-end

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import todoRoutes from "./routes/todo.route.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is Ready");
-});
+app.use(express.json());
+
+app.use("/api/todos", todoRoutes);
 
 app.listen(4000, () => {
   connectDB();
